333e5689aa182aa6b03a5f29f041625fca7a64f2
Lenny <valentin805@gmail.com>
2019-02-24 00:28:54 +0200

    Add Action on Error delegate.

--- UserLogin/UserLogin/LoginValidation.cs
+++ UserLogin/UserLogin/LoginValidation.cs
@@ -8,6 +8,7 @@ namespace UserLogin
 {
     class LoginValidation
     {
+        public delegate void ActionOnError(String errorMsg);
         static private UserRoles currentUserRole;
         static public UserRoles CurrentUserRole
         {
@@ -20,13 +21,15 @@ namespace UserLogin
                 currentUserRole = value;
             }
         }
+        private ActionOnError actionOnError;
         private String errorMessage;
         private String userName;
         private String password;
-        public LoginValidation(String userName, String password)
+        public LoginValidation(String userName, String password, ActionError actionOnError)
         {
             this.userName = userName;
             this.password = password;
+            this.actionOnError = actionOnError;
         }
         public bool ValidateUserInput(User user)
         {
@@ -37,24 +40,28 @@ namespace UserLogin
             if (emptyUserName)
             {
                 this.errorMessage = "Missing username.";
+                this.actionOnError(this.errorMessage);
                 user.Role = 0;
                 return false;
             }
             if (emptyPassword)
             {
                 this.errorMessage = "Missing password.";
+                this.actionOnError(this.errorMessage);
                 user.Role = 0;
                 return false;
             }
             if(this.userName.Length < 5)
             {
                 this.errorMessage = "Username length too short.";
+                this.actionOnError(this.errorMessage);
                 user.Role = 0;
                 return false;
             }
             if(this.password.Length < 5)
             {
                 this.errorMessage = "Password length too short.";
+                this.actionOnError(this.errorMessage);
                 user.Role = 0;
                 return false;
             }
@@ -62,6 +69,7 @@ namespace UserLogin
             if(findUser == null)
             {
                 this.errorMessage = "User not found.";
+                this.actionOnError(this.errorMessage);
                 user.Role = 0;
                 return false;
             }
