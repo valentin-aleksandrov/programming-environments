404272a7ca984e328804f23a7bfbdcd3497bb70f
Lenny <valentin805@gmail.com>
2019-02-24 01:23:22 +0200

    Create StudentData.cs

---
+++ UserLogin/StudentRepository/StudentData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRepository
+{
+    class StudentData
+    {
+        Student TestStudent { get; set }
+    }
+}
