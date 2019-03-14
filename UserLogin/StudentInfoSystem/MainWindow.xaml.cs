2d17c63d13417d371340d6cb6f1bcc2a1fb3b156
Lenny <valentin805@gmail.com>
2019-03-14 16:55:53 +0200

    Import lab1 & lab2 projects to StudentInfoSystem

--- UserLogin/StudentInfoSystem/MainWindow.xaml.cs
+++ UserLogin/StudentInfoSystem/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UserLogin;
+using StudentRepository;
 
 namespace StudentInfoSystem
 {
@@ -22,6 +24,8 @@ namespace StudentInfoSystem
     {
         public MainWindow()
         {
+           
+            
             InitializeComponent();
         }
     }
