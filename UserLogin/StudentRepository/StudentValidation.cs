8f89362b15ba828030e2b0439a7da8ffb9ece4f7
Lenny <valentin805@gmail.com>
2019-03-14 17:14:44 +0200

    Find student by user.

---
+++ UserLogin/StudentRepository/StudentValidation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserLogin;
+namespace StudentRepository
+{
+    public class StudentValidation
+    {
+        public static Student GetStudentDataByUser(User user)
+        {
+            if (user == null || user.FakNum == null)
+            {
+                Console.WriteLine("No such student");
+                return null;
+            }
+
+            Student student = StudentData.findStudentByFacNumber(user.FakNum);
+
+            if(student == null)
+            {
+                Console.WriteLine("No such student");
+                return null;
+            } else
+            {
+                return student;
+            }
+        }
+    }
+}
